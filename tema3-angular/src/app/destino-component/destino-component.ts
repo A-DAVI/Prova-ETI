cc820e8876716492f2d0038d10fb7615fcd0c797
A-DAVI <cassolidavi603@gmail.com>
2026-04-09 21:08:31 -0300

    Criando projeto Angular

---
+++ tema3-angular/src/app/destino-component/destino-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destino-component',
+  imports: [],
+  templateUrl: './destino-component.html',
+  styleUrl: './destino-component.css',
+})
+export class DestinoComponent {
+
+}
---
+++ tema3-angular/src/app/viagem-component/viagem-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viagem-component',
+  imports: [],
+  templateUrl: './viagem-component.html',
+  styleUrl: './viagem-component.css',
+})
+export class ViagemComponent {
+
+}
